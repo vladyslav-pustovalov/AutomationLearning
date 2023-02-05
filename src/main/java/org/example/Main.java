8d558a894139726fbf4c00372525e029fce76d7c
vladpustovalov <pustovalov.v.y@gmail.com>
2023-02-05 13:19:47 +0200

    my first webdriver request

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,14 @@
-package org.example;public class Main {
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.drive", "D:\\AQA\\WebDrivers\\chromedriver_win32chromedriver.exe");
+
+        WebDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("https://google.com");
+
     }
 }
